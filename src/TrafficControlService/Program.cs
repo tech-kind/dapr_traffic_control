e06da8d2b3f2514c4a672d2c8d4bbd4d0bee71ee
tech-kind <ken_hayakawa@tech-kind.biz>
2022-07-29 13:09:17 +0900

    Actorは使用しないのでいったんコメントアウト localhostのスペルミスを修正

--- src/TrafficControlService/Program.cs
+++ src/TrafficControlService/Program.cs
@@ -9,16 +9,16 @@ var daprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3600
 var daprGrpcPort = Environment.GetEnvironmentVariable("DARP_GRPC_PORT") ?? "60000";
 builder.Services.AddDaprClient(builder => builder
     .UseHttpEndpoint($"http://localhost:{daprHttpPort}")
-    .UseGrpcEndpoint($"http://localhot:{daprGrpcPort}"));
+    .UseGrpcEndpoint($"http://localhost:{daprGrpcPort}"));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-builder.Services.AddActors(options =>
-{
-    options.Actors.RegisterActor<VehicleActor>();
-});
+//builder.Services.AddActors(options =>
+//{
+//    options.Actors.RegisterActor<VehicleActor>();
+//});
 
 var app = builder.Build();
 
@@ -31,6 +31,6 @@ if (app.Environment.IsDevelopment())
 app.UseCloudEvents();
 
 app.MapControllers();
-app.MapActorsHandlers();
+// app.MapActorsHandlers();
 
 app.Run("http://localhost:6000");
